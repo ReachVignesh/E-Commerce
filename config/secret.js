30cf68cbe1ad68223d891d16366fcc7197580fa4
ReachVignesh <rajendrenvignesh@gmail.com>
2016-04-20 19:08:53 +0100

    Built Category and Product pages

--- config/secret.js
+++ config/secret.js
@@ -1,6 +1,10 @@
 module.exports = {
 
-  database: 'mongodb://root:viki123@ds019668.mlab.com:19668/ecommerce',
+  //database: 'mongodb://root:viki123@ds019668.mlab.com:19668/ecommerce',
+  //port: 3000,
+  //secretKey: "Viki!@$#%#"
+
+  database: 'mongodb://root:abc123@ds037095.mongolab.com:37095/ecommerce',
   port: 3000,
-  secretKey: "Viki!@$#%#"
+  secretKey: "Arash@$@!#@"
 }
